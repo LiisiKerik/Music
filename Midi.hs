cad6d826140f1e28ad36cbca53d19f933781eb99
Liisi Kerik <liisikerik@hotmail.com>
2018-03-14 19:28:19 +0200

    -

--- Midi.hs
+++ Midi.hs
@@ -4,11 +4,13 @@ module Midi where
   import Data.Bifunctor
   import Data.Bits
   import Data.ByteString
+  import Data.Int
+  import Data.List
+  import Data.Map
   import Data.Word
-  data Composition = Composition Word8 Word8 [Melody_track] [Percussion_event] deriving Show
+  data Composition = Composition Word8 Word8 [Word8] [Melody] [Percussion_event] deriving Show
   type Err t = Either String t
-  data Event = End | Start deriving Show
-  data Event' = Event' Word8 Event Word8 deriving Show
+  data Event = Event Word8 [Int8] [Int8] deriving Show
   data Melodic_instrument =
     Bells |
     Cello |
@@ -24,8 +26,8 @@ module Midi where
     Violin |
     Voice
       deriving Show
-  data Melody_track = Melody_track Melodic_instrument [Event'] deriving Show
-  data Percussion_event = Percussion_event Word8 Percussion_instrument deriving Show
+  data Melody = Melody Melodic_instrument [Event] deriving Show
+  data Percussion_event = Percussion_event Word8 [Percussion_instrument] deriving Show
   data Percussion_instrument =
     Bass_drum_1 |
     Bass_drum_2 |
@@ -54,7 +56,8 @@ module Midi where
     Tambourine |
     Vibra_slap
       deriving (Eq, Show)
-  data Track = Track Word8 [Word8] [Event'] deriving Show
+  data Track = Track Word8 [Word8] [Event] deriving Show
+  data Velocities = Velocities Word8 (Map Word8 Word8) deriving Show
   bytes :: Word8 -> Word32 -> [Word8]
   bytes = bytes' []
   bytes' :: [Word8] -> Word8 -> Word32 -> [Word8]
@@ -63,89 +66,108 @@ module Midi where
       0 -> x
       _ -> bytes' (fromIntegral (mod y 256) : x) (n - 1) (div y 256)
   encode :: Composition -> Err [Word8]
-  encode (Composition breakdown tempo melody percussion) =
+  encode (Composition breakdown tempo velocities melody percussion) =
     if breakdown < 8
       then
         if tempo < 4
           then Left "Tempo should be at least 4 beats per minute."
           else
-            (
-              encode_melody [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15] melody >>=
-              \(tracks, melody') ->
+            case velocities of
+              [] -> Left "The list of velocities should not be empty."
+              velocity : velocities' ->
                 (
-                  (++) [77, 84, 104, 100, 0, 0, 0, 6, 0, 1, 0, tracks, 0, shiftL 1 (fromIntegral breakdown)] <$>
-                  encode_tracks
+                  velocity_map (Velocities 1 (Data.Map.singleton 0 velocity)) velocities' >>=
+                  \velocities'' ->
                     (
-                      Track
-                        9
-                        ([0, 255, 81, 3] ++ bytes 3 (div 60000000 (fromIntegral tempo)))
-                        (encode_percussion (start <$> percussion)) :
-                      melody')))
+                      encode_melody (Data.List.delete 9 [0 .. 15]) melody >>=
+                      \(tracks, melody') ->
+                        (
+                          (++) [77, 84, 104, 100, 0, 0, 0, 6, 0, 1, 0, tracks, 0, shiftL 1 (fromIntegral breakdown)] <$>
+                          encode_tracks
+                            velocities''
+                            (
+                              Track
+                                9
+                                ([0, 255, 81, 3] ++ bytes 3 (div 60000000 (fromIntegral tempo)))
+                                (encode_percussion [] percussion) :
+                              melody'))))
       else Left "Quarternote breakdown should be no more than 1 / 2 ^ 7."
-  encode_event :: Word8 -> Event' -> Err [Word8]
-  encode_event channel (Event' time event note) =
-    if note < 128
-      then Right (encode_time time ++ [event_number event + channel, note, 127])
-      else Left "The range of notes is {0,...,127}."
-  encode_events :: Word8 -> [Event'] -> Err [Word8]
-  encode_events channel = map_and_cat (encode_event channel)
-  encode_melody :: [Word8] -> [Melody_track] -> Err (Word8, [Track])
+  encode_event :: Velocities -> Word8 -> Word8 -> Event -> Err (Word8, [Word8])
+  encode_event (Velocities bar_length velocities) channel time (Event time' end start) =
+    let
+      time'' = mod (time + time') bar_length
+      encode_my_event = encode_event' channel (unsafe_lookup velocities time'')
+    in
+      (
+        (,) time'' <$> 
+        (case end of
+          [] ->
+            case start of
+              [] -> Left "Empty event."
+              note : start' ->
+                encode_my_event time' 144 note >>= \note' -> (++) note' <$> map_and_cat (encode_my_event 0 144) start'
+          note : end' ->
+            (
+              encode_my_event time' 128 note >>=
+              \note' ->
+                (
+                  map_and_cat (encode_my_event 0 128) end' >>=
+                  \end'' -> (++) (note' ++ end'') <$> map_and_cat (encode_my_event 0 144) start))))
+  encode_event' :: Word8 -> Word8 -> Word8 -> Word8 -> Int8 -> Err [Word8]
+  encode_event' channel velocity time event note =
+    if note < 0
+      then Left "Notes should not be negative."
+      else Right (encode_time time ++ [event + channel, fromIntegral note, velocity])
+  encode_events :: Velocities -> Word8 -> Word8 -> [Event] -> Err [Word8]
+  encode_events velocities channel time events =
+    case events of
+      [] -> Right []
+      event : events' ->
+        (
+          encode_event velocities channel time event >>=
+          \(time', event') -> (++) event' <$> encode_events velocities channel time' events')
+  encode_melody :: [Word8] -> [Melody] -> Err (Word8, [Track])
   encode_melody channels melodies =
     case melodies of
       [] -> Right (1, [])
-      Melody_track instrument melody : melodies' ->
+      Melody instrument melody : melodies' ->
         case channels of
           [] -> Left "The number of melody tracks is limited to 15."
           channel : channels' ->
             (
-              bimap ((+) 1) ((:) (Track channel [0, 192 + channel, melodic_instrument instrument] melody)) <$>
+              bimap
+                ((+) 1)
+                ((:)
+                  (Track
+                    channel
+                    [0, 192 + channel, melodic_instrument instrument]
+                    melody)) <$>
               encode_melody channels' melodies')
-  encode_percussion :: [Event'] -> [Event']
-  encode_percussion percussion =
+  encode_percussion :: [Int8] -> [Percussion_event] -> [Event]
+  encode_percussion end percussion =
     case percussion of
-      [] -> []
-      event' @ (Event' _ event instrument) : percussion' ->
-        (
-          event' :
-          encode_percussion
-            (case event of
-              End -> percussion'
-              Start -> insert_end 4 instrument percussion'))
+      [] ->
+        case end of
+          [] -> []
+          _ -> [Event 5 end []]
+      Percussion_event time instruments : percussion' ->
+        let
+          instruments' = percussion_instrument <$> instruments
+        in
+          Event time end instruments' : encode_percussion instruments' percussion'
   encode_time :: Word8 -> [Word8]
   encode_time time = if time < 128 then [time] else [129, time - 128]
-  encode_track :: Track -> Err [Word8]
-  encode_track (Track channel header events) =
+  encode_track :: Velocities -> Track -> Err [Word8]
+  encode_track velocities (Track channel header events) =
     (
       (\events' ->
         let
           track = header ++ events' ++ [0, 255, 47, 0]
         in
           [77, 84, 114, 107] ++ bytes 4 (fromIntegral (Prelude.length track)) ++ track) <$>
-      encode_events channel events)
-  encode_tracks :: [Track] -> Err [Word8]
-  encode_tracks = map_and_cat encode_track
-  event_number :: Event -> Word8
-  event_number event =
-    case event of
-      End -> 128
-      Start -> 144
-  insert_end :: Word8 -> Word8 -> [Event'] -> [Event']
-  insert_end time instrument track =
-    let
-      end_as_planned = Event' time End instrument
-    in
-      case track of
-        [] -> [end_as_planned]
-        event' @ (Event' time' event instrument') : track' ->
-          if time' < time
-            then
-              if instrument' == instrument
-                then
-                  case event of
-                    End -> track
-                    Start -> Event' time' End instrument : Event' 0 Start instrument : track'
-                else event' : insert_end (time - time') instrument track'
-            else end_as_planned : Event' (time' - time) event instrument' : track'
+      encode_events velocities channel 0 events)
+  encode_tracks :: Velocities -> [Track] -> Err [Word8]
+  encode_tracks velocities = map_and_cat (encode_track velocities)
   map_and_cat :: Monad f => (t -> f [u]) -> [t] -> f [u]
   map_and_cat f x = Prelude.concat <$> mapM f x
   melodic_instrument :: Melodic_instrument -> Word8
@@ -169,7 +191,7 @@ module Midi where
     case encode composition of
       Left err -> Prelude.putStrLn err
       Right encoding -> Data.ByteString.writeFile (file ++ ".mid") (pack encoding)
-  percussion_instrument :: Percussion_instrument -> Word8
+  percussion_instrument :: Percussion_instrument -> Int8
   percussion_instrument instrument =
     case instrument of
       Bass_drum_1 -> 36
@@ -198,6 +220,20 @@ module Midi where
       Side_stick -> 37
       Tambourine -> 54
       Vibra_slap -> 58
-  start :: Percussion_event -> Event'
-  start (Percussion_event time instrument) = Event' time Start (percussion_instrument instrument)
+  unsafe_lookup :: Ord t => Map t u -> t -> u
+  unsafe_lookup m x =
+    case Data.Map.lookup x m of
+      Just y -> y
+      Nothing -> undefined
+  velocity_map :: Velocities -> [Word8] -> Err Velocities
+  velocity_map velocities @ (Velocities time velocities') velocities_0 =
+    case time of
+      0 -> Left "The list of velocities is too long."
+      _ ->
+        case velocities_0 of
+          [] -> Right velocities
+          velocity : velocities_1 ->
+            if velocity < 128
+              then velocity_map (Velocities (time + 1) (Data.Map.insert time velocity velocities')) velocities_1
+              else Left "Velocities should be in range {0, ..., 127}."
 -----------------------------------------------------------------------------------------------------------------------------
